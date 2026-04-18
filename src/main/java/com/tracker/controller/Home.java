8b7418486a0fbe2eabd54e0680ebc2562b462ce5
Emile <emorris7060@gmail.com>
2026-04-17 19:33:26 -0500

    Completed form display.

--- src/main/java/com/tracker/controller/Home.java
+++ src/main/java/com/tracker/controller/Home.java
@@ -1,5 +1,8 @@
 package com.tracker.controller;
 
+import com.tracker.entity.Job;
+import com.tracker.entity.User;
+import com.tracker.persistence.JobDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +13,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Handles forwarding authenticated users to the home page.
@@ -19,6 +23,12 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/home")
 public class Home extends HttpServlet {
     private static final Logger log = LogManager.getLogger(Home.class);
+    private JobDao jobDao;
+
+    @Override
+    public void init() throws ServletException {
+        jobDao = new JobDao();
+    }
 
     /**
      * Forwards authenticated users to the home page.
@@ -44,8 +54,14 @@ public class Home extends HttpServlet {
             return;
         }
 
+        // Load saved user data from the session
+        User dbUser = (User) session.getAttribute("dbUser");
+        List<Job> jobs = jobDao.findByUserId(dbUser.getId());
+        req.setAttribute("jobs", jobs);
+
         log.info("Authenticated user forward back to home");
         session.setAttribute("page", "Home - Job Tracker");
+
         req.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(req, resp);
     }
 }
